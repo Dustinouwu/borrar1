5794e1fc4ca0795734db0e33215b246b92c05f50
Dustinouwu <dastin.chavez@epn.edu.ec>
2023-04-11 21:58:16 -0500

    comienzo de carrusel

--- src/partials/FeaturesZigzag.jsx
+++ src/partials/FeaturesZigzag.jsx
@@ -144,7 +144,7 @@ function FeaturesZigzag() {
             <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
               {/* Image */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
-                <img className="max-w-full mx-auto md:max-w-none h-auto" src={poster5} width="540" height="405" alt="Features 03" />
+                <img className="max-w-full mx-auto md:max-w-none h-auto" src={poster5} alt="Features 03" />
               </div>
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
@@ -175,10 +175,7 @@ function FeaturesZigzag() {
                 </div>
               </div>
             </div>
-
-          </div>
-           {/* 4nd item */}
-           <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
               {/* Image */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
                 <img className="max-w-full mx-auto md:max-w-none h-auto" src={poster6} width="540" height="405" alt="Features 02" />
@@ -190,7 +187,7 @@ function FeaturesZigzag() {
                   <h3 className="h3 mb-3">Manteniemiento Preventivo del Vehículo</h3>
                   <p className="text-xl text-gray-400 mb-4">Es cualquier mantenimiento que esté diseñado para conservar la condición adecuada del equipo y prevenir fallas, como:</p>
                   <ul className="text-lg text-gray-400 -mb-2">
-                    
+
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
@@ -245,11 +242,15 @@ function FeaturesZigzag() {
                       </svg>
                       <span>Bujias, ABC del motor</span>
                     </li>
-                    
+
                   </ul>
                 </div>
               </div>
             </div>
+
+          </div>
+          {/* 4nd item */}
+
 
         </div>
       </div>
